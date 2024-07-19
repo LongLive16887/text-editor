cbba19730ba18835dcfbed351539b84230289eb9
Shohruh L.L <shohruhllj@gmail.com>
2024-07-19 18:14:44 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+const parseLines = (text) => {
+  const lines = text.split('\n');
+  const nonEmptyLines = lines
+        .map(line => line.trim())
+        .filter(line => line !== '')
+        .map(line => line.match(/@(\w+)/g));
+  console.log(nonEmptyLines);
+  let newnonEmptyLines = nonEmptyLines.filter((x) => x != null);
+  console.log(newnonEmptyLines)
+  let results = [];
+  
+  for(let x = 0; x < 32; x+=2){
+    results.push(`${x/2+1}. ${newnonEmptyLines[x]} vs ${newnonEmptyLines[x+1]}`)
+  }
+
+  return(results);
+}
 
 function App() {
+  const [data, setData] = useState(''); // Инициализируем пустой строкой
+  const [results, setResults] = useState([]); // Инициализируем пустым массивом
+
+  const handleInputChange = ({ target: { value } }) => {
+    setData(value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await parseLines(data);
+    setResults(result);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={handleSubmit}>
+          <textarea
+              className="form-field"
+              type="text"
+              name="usernames"
+              placeholder="text"
+              value={data}
+              onChange={handleInputChange}
+          />
+          <button type="submit">
+              Отправить
+          </button>
+        </form>
+        <div className="results">
+          {results.length > 0 ? (
+            results.map((line, index) => <p key={index}>{line}</p>)
+          ) : null}
+        </div>
     </div>
   );
 }
